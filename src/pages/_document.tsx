e48dac3a3b836a5c76c5c66f6bfb8b53778989e9
yude <i@yude.jp>
2023-02-25 13:21:59 +0000

    chore: add js libraries

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,8 @@ export default function Document() {
 
         <Main />
         <NextScript />
+        <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></script>
       </body>
     </Html>
   )
