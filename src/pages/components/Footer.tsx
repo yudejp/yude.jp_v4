c5f03096367e4d2bbb329df4f204e90c297898c3
yude <i@yude.jp>
2023-02-26 23:33:14 +0000

    feat: show git hash

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -3,22 +3,19 @@ import Image from "next/image"
 
 export default function Footer() {
     return (
-        <div className="text-center">
-            &copy; 2023 yude. (<Link href="https://github.com/yudejp/yude.jp_v4/blob/main/README.md">ライセンス</Link>)
-            <div>
-                <div className="d-inline m-1">
-                    <a href="https://sites.google.com/site/happybusy/">
-                        <Image src="/assets/images/busy_banner.png" width={200} height={40} alt="happybusy" />
-                    </a>
-                </div>
-                <div className="d-inline m-1">
-                    <a href="https://count.getloli.com/get/@yude?theme=asoul">
-                        <img src="https://count.getloli.com/get/@yude?theme=asoul"
-                            alt="表示カウンター (count.getloli.com)"
-                            loading="lazy" />
-                    </a>
-                </div>
+        <>
+            <span className="text-center d-block">&copy; 2023 yude. (<Link href="https://github.com/yudejp/yude.jp_v4/blob/main/README.md">ライセンス</Link>)</span>
+            <span className="text-center d-block">Revision: <code>{process.env.VERCEL_GIT_COMMIT_SHA || "dev"}</code></span>
+            <div className="text-center">
+                <a href="https://sites.google.com/site/happybusy/">
+                    <Image src="/assets/images/busy_banner.png" width={200} height={40} alt="happybusy" />
+                </a>
+                <a href="https://count.getloli.com/get/@yude?theme=asoul">
+                    <img src="https://count.getloli.com/get/@yude?theme=asoul"
+                        alt="表示カウンター (count.getloli.com)"
+                        loading="lazy" />
+                </a>
             </div>
-        </div>
+        </>
     )
 }
