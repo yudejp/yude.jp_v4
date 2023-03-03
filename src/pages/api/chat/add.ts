082f147b8956410fd99f1d38a6e16c6c83f30c1f
yude <i@yude.jp>
2023-03-03 07:56:33 +0000

    feat: add rate-limit on chat

--- src/pages/api/chat/add.ts
+++ src/pages/api/chat/add.ts
@@ -1,12 +1,26 @@
 import { db } from "../../../lib/firebase";
 import { collection, addDoc } from "firebase/firestore";
+import { LimitChecker } from "@/lib/limitChecker";
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+const limitChecker = LimitChecker();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const message = req.query.message;
     const ip_addr = req.headers['x-real-ip'] || req.connection.remoteAddress;
     const port = req.headers['x-real-port'] || req.connection.remotePort;
 
+    try {
+        await limitChecker.check(res, 4, req.connection.remoteAddress || "IP_NOT_FOUND");
+    } catch (error) {
+        console.error(error);
+
+        res.status(429).json({
+            text: `Rate Limited`,
+            clientIp: req.connection.remoteAddress,
+        });
+        return;
+    }
     if (message) {
         await addDoc(collection(db, "messages"), {
             message: message,
