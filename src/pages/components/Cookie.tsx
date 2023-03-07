ca3cf3e4e9e2f0a428c19bd8009ff5c6f6bf7ea6
yude <i@yude.jp>
2023-03-07 23:21:22 +0000

    feat: :art:

--- src/pages/components/Cookie.tsx
+++ src/pages/components/Cookie.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import type { CounterData } from "@/types/counter";
 import Head from "next/head"
+import { motion } from "framer-motion"
 
 export default function ChatList() {
     const [cookie, setCookie] = useState<CounterData>({ name: "counter", num: 0 });
@@ -26,7 +27,18 @@ export default function ChatList() {
                 <link href="https://fonts.googleapis.com/css2?family=Rampart+One&display=swap" rel="stylesheet" />
             </Head>
             <div className="card-body text-center">
-                <span className="d-block" style={{ fontSize: 200, userSelect: "none", margin: -70 }} onClick={handleSubmit}>🍪</span>
+                <span className="d-block" style={{ fontSize: 200, userSelect: "none", margin: -70 }} onClick={handleSubmit}>
+                    <motion.div
+                        whileHover={{ scale: 1.2, rotate: 90 }}
+                        whileTap={{
+                            scale: 0.8,
+                            rotate: -90,
+                            borderRadius: "100%"
+                        }}
+                    >
+                        🍪
+                    </motion.div>
+                </span>
                 <span className="d-block" style={{ fontFamily: 'Rampart One', fontSize: 70 }}>{!isLoading && cookie && cookie.num}{isLoading && cookie.num}</span>
             </div>
         </div>
