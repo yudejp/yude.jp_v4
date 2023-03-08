38445481e927d172cceca195b02446d72036134f
yude <i@yude.jp>
2023-03-08 10:11:50 +0000

    fix: :art: @ quotes

--- src/pages/components/Quotes.tsx
+++ src/pages/components/Quotes.tsx
@@ -25,8 +25,9 @@ export default function Quotes() {
     }, [])
 
     return (
-        <div className="text-center m-4">
-            <span className="fs-4 d-block" style={{ fontFamily: "serif" }}>{data && data.text}</span>
+        <div className="position-relative text-center mt-2 mb-2 text-warning shadow-lg p-2 rounded" style={{ backgroundImage: `url("./assets/images/marisa.png")`, backgroundPositionY: "300px" }}>
+            <span className="position-absolute text-secondary" style={{ bottom: "3px", right: "3px" }}>絵: 猫蝉 さん (<a href="https://twitter.com/NekoSemi96" style={{ color: "currentcolor" }}>@NekoSemi96</a>)</span>
+            <span className="fs-4 d-block" style={{ fontFamily: "serif", textShadow: `1px 1px 3px #ffd960` }}>{data && data.text}</span>
             <span className="d-block">{data && data.artist} / {data && data.title}</span>
             <button type="button" className="btn btn-outline-info" onClick={handleClick}><FontAwesomeIcon icon={faRefresh} width={30} /></button>
         </div>
