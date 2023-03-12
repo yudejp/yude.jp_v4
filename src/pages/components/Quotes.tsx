b614b4637ba34b49971f93ab3a43a21762d66b52
yude <i@yude.jp>
2023-03-12 07:03:40 +0000

    fix: :art:

--- src/pages/components/Quotes.tsx
+++ src/pages/components/Quotes.tsx
@@ -25,9 +25,9 @@ export default function Quotes() {
     }, [])
 
     return (
-        <div className="position-relative text-center mt-2 mb-2 text-warning shadow-lg p-2 rounded" style={{ backgroundImage: `url("./assets/images/marisa.png")`, backgroundPositionY: "300px" }}>
+        <div className="position-relative text-center mt-2 mb-2 shadow-lg p-2 rounded text-info" style={{ backgroundImage: `url("./assets/images/marisa.png")`, backgroundPositionY: "400px", backgroundColor: `rgba(255, 255, 255, 0.8)`, backgroundBlendMode: "lighten" }}>
             <span className="position-absolute text-secondary" style={{ bottom: "3px", right: "3px" }}>絵: 猫蝉 さん (<a href="https://twitter.com/NekoSemi96" style={{ color: "currentcolor" }}>@NekoSemi96</a>)</span>
-            <span className="fs-4 d-block" style={{ fontFamily: "serif", textShadow: `1px 1px 3px #ffd960` }}>{data && data.text}</span>
+            <span className="fs-4 d-block fw-bold" style={{ fontFamily: "serif", textShadow: `1px 1px 3px #9ADDFC` }}>{data && data.text}</span>
             <span className="d-block">{data && data.artist} / {data && data.title}</span>
             <button type="button" className="btn btn-outline-info" onClick={handleClick}><FontAwesomeIcon icon={faRefresh} width={30} /></button>
         </div>
