555e86aa5a79f3e39a91b7ca31b0580f4cc96be3
yude <i@yude.jp>
2024-01-23 08:26:05 +0900

    Delete microCMS function

--- src/pages/pages.tsx
+++ src/pages/pages.tsx
@@ -1,42 +1,12 @@
-import { createClient } from "microcms-js-sdk";
-import type { Blog } from "@/types/blog";
-
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar, faTags } from "@fortawesome/free-solid-svg-icons";
-
 import Title from "./components/Title"
 
-export const getStaticProps = async () => {
-    const microcms_key = process.env.MICROCMS_KEY != undefined ? process.env.MICROCMS_KEY : '';
 
-    const client = createClient({
-        serviceDomain: 'yude',
-        apiKey: microcms_key,
-    });
-
-    const blog = await client.get({ endpoint: "page" });
-
-    return {
-        props: {
-            blogs: blog.contents,
-        },
-    };
-};
-
-function BlogPage({ blogs }: { blogs: Blog[] }) {
+function BlogPage() {
     return (
         <div>
             <Title title="ページ一覧" />
             <p className="fs-2">ページ一覧</p>
             <div className="container" style={{minHeight: "300px"}}>
-                <ul>
-                    {
-                        blogs.map((blog) => (
-                            <li key={blog.id}><Link href={"pages/" + blog.id} suppressHydrationWarning>{blog.title}</Link></li>
-                        ))
-                    }
-                </ul>
             </div>
         </div>
     );
